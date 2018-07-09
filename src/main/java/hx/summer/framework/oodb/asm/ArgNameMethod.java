6d7ebf8f2938f7acabe9101fa9f32041184b13ff
andyliang <2825523288@qq.com>
2018-07-09 21:05:41 +0800

    轻量级ORM框架

---
+++ src/main/java/hx/summer/framework/oodb/asm/ArgNameMethod.java
@@ -0,0 +1,144 @@
+/**
+ *    Copyright 2009-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package hx.summer.framework.oodb.asm;
+
+import org.objectweb.asm.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author mingliang
+ * @Date 2018-06-11 17:30
+ */
+public class ArgNameMethod {
+
+    /**
+     * 获取方法参数名
+     * @param method
+     * @return
+     */
+    public static List<String> methodArgNames(final Method method){
+        final String n = method.getDeclaringClass().getName();
+        try {
+            ClassReader cr = new ClassReader(n);
+            ArgNameClassVisitor argNameClassVisitor=new ArgNameClassVisitor();
+            cr.accept(argNameClassVisitor, 0);
+            return argNameClassVisitor.getArgumentNames();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.EMPTY_LIST;
+    }
+
+    public static Map<String,Class<?>> methodArgTypes(final Method method){
+        final String n = method.getDeclaringClass().getName();
+        try {
+            ClassReader cr = new ClassReader(n);
+            ArgNameClassVisitor argNameClassVisitor=new ArgNameClassVisitor();
+            cr.accept(argNameClassVisitor, 0);
+            return argNameClassVisitor.getArgTypeMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *
+     * <p>
+     * 获取方法的参数名
+     * </p>
+     *
+     * @param m
+     * @return
+     */
+    public static String[] getMethodParamNames(final Method m) {
+        final String[] paramNames = new String[m.getParameterTypes().length];
+        final String n = m.getDeclaringClass().getName();
+        ClassReader cr = null;
+        try {
+            cr = new ClassReader(n);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        cr.accept(new ClassVisitor(Opcodes.ASM4) {
+            @Override
+            public MethodVisitor visitMethod(final int access,
+                                             final String name, final String desc,
+                                             final String signature, final String[] exceptions) {
+                final Type[] args = Type.getArgumentTypes(desc);
+                // 方法名相同并且参数个数相同
+                if (!name.equals(m.getName()) || !sameType(args, m.getParameterTypes())) {
+                    return super.visitMethod(access, name, desc, signature, exceptions);
+                }
+                MethodVisitor v = super.visitMethod(access, name, desc, signature, exceptions);
+                return new MethodVisitor(Opcodes.ASM4, v) {
+                    @Override
+                    public void visitLocalVariable(String name, String desc,String signature, Label start, Label end, int index) {
+                        int i = index - 1;
+                        // 如果是静态方法，则第一就是参数
+                        // 如果不是静态方法，则第一个是"this"，然后才是方法的参数
+                        if (Modifier.isStatic(m.getModifiers())) {
+                            i = index;
+                        }
+                        if (i >= 0 && i < paramNames.length) {
+                            paramNames[i] = name;
+                        }
+                        super.visitLocalVariable(name, desc, signature, start, end, index);
+                    }
+
+                };
+            }
+        }, 0);
+        return paramNames;
+    }
+
+    /**
+     *
+     * <p>
+     * 比较参数类型是否一致
+     * </p>
+     *
+     * @param types
+     *            asm的类型({@link Type})
+     * @param clazzes
+     *            java 类型({@link Class})
+     * @return
+     */
+    private static boolean sameType(Type[] types, Class<?>[] clazzes) {
+        // 个数不同
+        if (types.length != clazzes.length) {
+            return false;
+        }
+
+        for (int i = 0; i < types.length; i++) {
+            if (!Type.getType(clazzes[i]).equals(types[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        Method method = TestClass.class.getDeclaredMethod("getName",String.class,int.class,List.class);
+        methodArgNames(method);
+    }
+}
---
+++ src/main/java/hx/summer/framework/oodb/asm/ArgNameMethodVisitor.java
@@ -0,0 +1,68 @@
+/**
+ *    Copyright 2009-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package hx.summer.framework.oodb.asm;
+
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+
+import java.util.List;
+
+/**
+ * @Author mingliang
+ * @Date 2018-06-11 17:26
+ */
+public class ArgNameMethodVisitor extends MethodVisitor {
+    /** 变量名 */
+    private List<String> variables;
+    private List<String> argumentNames;
+    private int argLen; //变量个数
+
+    public ArgNameMethodVisitor(int api,List<String> argumentNames,int argLen) {
+        super(api);
+        this.argumentNames=argumentNames;
+        this.argLen=argLen;
+    }
+
+    /**
+     * asm遍历局部变量
+     * @param name
+     * @param desc
+     * @param signature
+     * @param start
+     * @param end
+     * @param index
+     */
+    @Override
+    public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index) {
+        //如果是this变量，则掠过
+        if("this".equals(name)) {
+            return;
+        }
+
+        System.out.println("name = "+name);
+
+        if(argLen-- > 0) {
+            System.out.println("name = "+name);
+            argumentNames.add(name);
+        }
+    }
+
+    @Override
+    public void visitParameter(String name, int access) {
+//        super.visitParameter(name, access);
+        System.out.println("paramName = "+name);
+    }
+}
